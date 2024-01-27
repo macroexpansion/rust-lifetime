b045afc1fb485b9d1ddad9e547ee6ba845fca0b0
macroexpansion <trasuadev@gmail.com>
2024-01-27 18:24:58 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Note: debug expects two parameters with the *same* lifetime
+fn debug<'a: 'b, 'b>(a: &'a str, b: &'b str) -> &'b str {
+    if a.len() > b.len() {
+        a
+    } else {
+        b
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hello = String::from("hello"); // 'hello
+    {
+        let world = String::from("world");
+        let world = &world; // 'world has a shorter lifetime than 'hello
+        debug(&hello, world);
+    }
 }
